61dc522b8209d47c69745b206d3b214bed4ea5c0
Егор Кочнев <miss.oksana1977@yandex.ru>
2021-11-30 19:17:18 +0800

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lab5.Objects;
+
+namespace Lab5
+{
+    public partial class Form1 : Form
+    {
+        MyRectangle myRect;
+        public Form1()
+        {
+            InitializeComponent();
+            myRect = new MyRectangle(0, 0, 0);
+        }
+
+        private void pbMain_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+
+            g.Clear(Color.White);
+
+            myRect.Render(g);
+        }
+    }
+}
