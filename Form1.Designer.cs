227a0a208bd264baae719284afbdd60c9d5b42fd
Егор Кочнев <miss.oksana1977@yandex.ru>
2021-12-03 21:31:05 +0800

    Guide

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,24 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pbMain = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtLog = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pbMain)).BeginInit();
             this.SuspendLayout();
             // 
             // pbMain
             // 
-            this.pbMain.Location = new System.Drawing.Point(-1, -2);
+            this.pbMain.Location = new System.Drawing.Point(4, 1);
             this.pbMain.Name = "pbMain";
-            this.pbMain.Size = new System.Drawing.Size(484, 463);
+            this.pbMain.Size = new System.Drawing.Size(644, 435);
             this.pbMain.TabIndex = 0;
             this.pbMain.TabStop = false;
             this.pbMain.Paint += new System.Windows.Forms.PaintEventHandler(this.pbMain_Paint);
+            this.pbMain.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pbMain_MouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(654, 1);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(213, 435);
+            this.txtLog.TabIndex = 1;
+            this.txtLog.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.ClientSize = new System.Drawing.Size(870, 461);
+            this.Controls.Add(this.txtLog);
             this.Controls.Add(this.pbMain);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,5 +76,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pbMain;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox txtLog;
     }
 }
--- Objects/BaseObject.cs
+++ Objects/BaseObject.cs
@@ -4,14 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace Lab5.Objects
 {
     class BaseObject
     {
+        public Action<BaseObject, BaseObject> OnOverlap;
+
         public float X;
         public float Y;
         public float Angle;
+
+        public virtual GraphicsPath GetGraphicsPath()
+        {
+            return new GraphicsPath();
+        }
+
+        public virtual bool Overlaps(BaseObject obj, Graphics g)
+        {        
+            var path1 = this.GetGraphicsPath();
+            var path2 = obj.GetGraphicsPath();
+
+            path1.Transform(this.GetTransform());
+            path2.Transform(obj.GetTransform());
+
+            var region = new Region(path1);
+            region.Intersect(path2);
+            return !region.IsEmpty(g);
+        }        
+
+        public virtual void Overlap (BaseObject obj)
+        {
+            if (this.OnOverlap != null)
+            {
+                this.OnOverlap(this, obj);
+            }
+        }
 
         public BaseObject(float x, float y, float angle)
         {
@@ -20,6 +49,15 @@ namespace Lab5.Objects
             Angle = angle;
         }
        
+        public Matrix GetTransform()
+        {
+            var matrix = new Matrix();
+            matrix.Translate(X, Y);
+            matrix.Rotate(Angle);
+
+            return matrix;
+        }
+
         public virtual void Render(Graphics g)
         {
             
